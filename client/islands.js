281fc9eab849f9bc8d4fc6d7c1aca6f3e3c31f95
Max Duval <max.duval@theguardian.com>
2022-10-15 12:12:55 +0100

    feat: type-check javascript files

--- client/islands.js
+++ client/islands.js
@@ -1,6 +1,7 @@
+/** @type {(element: Element) => Promise<void>} */
 const hydrate = async (element) => {
   const name = element.getAttribute("name");
-  const props = JSON.parse(element.getAttribute("props"));
+  const props = JSON.parse(element.getAttribute("props") ?? "{}");
   const load = performance.now();
   const { default: Component } = await import(`/${name}.js`);
   console.info(`Loaded ${name} in ${performance.now() - load}ms`, props);
@@ -14,6 +15,4 @@ const hydrate = async (element) => {
   console.info(`Hydrated ${name} in ${performance.now() - hydrate}ms`);
 };
 
-for (const island of document.querySelectorAll("island[name]")) {
-  hydrate(island);
-}
+document.querySelectorAll("one-claw[name]").forEach(hydrate);

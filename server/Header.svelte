c91be809a2567e6bd338b58101161c91f9669a13
Max Duval <max.duval@theguardian.com>
2022-10-16 14:03:26 +0100

    feat: add claw logo

--- server/Header.svelte
+++ server/Header.svelte
@@ -1,8 +1,12 @@
 <script>
+  import Logo from "./Logo.svelte";
+
   const items = ["One", "Two", "Three"];
 </script>
 
 <header>
+  <Logo colour={"darkslategrey"} />
+  <h1>Mononykus</h1>
   <nav>
     <ul>
       {#each items as item}
@@ -15,11 +19,22 @@
 <style>
   header {
     background-color: antiquewhite;
+    display: grid;
+    grid-template-columns: 32px 2fr 1fr;
+    gap: 0.5rem;
+  }
+
+  h1 {
+    color: darkslategrey;
+    font-size: 1.5rem;
+    margin: 0;
   }
 
   ul {
     display: grid;
     grid-template-columns: repeat(3, 100px);
+    margin: 0;
+    padding: 0;
   }
 
   li {

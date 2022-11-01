9062d88b30512b7364700cac26601fb9679d2c5a
Max Duval <hi@mxdvl.com>
2022-11-01 09:41:02 +0000

    feat: add Island types

--- server/Island.svelte
+++ server/Island.svelte
@@ -2,7 +2,9 @@
   import Counter from "../shared/Counter.svelte";
   import Name from "../shared/Name.svelte";
 
+  /** @type {keyof islands} */
   export let name;
+  /** @type {Record<string, string | number | boolean | null>} */
   export let props;
 
   /** @TODO This should be auto-generated */

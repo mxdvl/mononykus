ebdba96a4c5ac9af00427a9efdb71d7152bead4a
Alex Sanders <alex@sndrs.dev>
2023-04-20 18:27:43 +0100

    gropu hydration logs

--- src/esbuild_plugins/build_routes/get_route_html.ts
+++ src/esbuild_plugins/build_routes/get_route_html.ts
@@ -7,23 +7,21 @@ interface TemplateOptions {
 	html: string;
 	hydrate: string;
 }
-const template = ({ css, head, html, hydrate }: TemplateOptions) =>
-	`<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    
-	${head}
-
-    <script type="module">${hydrate}</script>
-    <style>${css}</style>
-  </head>
-  <body>
-    ${html}
-  </body>
-</html>
+const template = ({ css, head, html, hydrate }: TemplateOptions) => `
+	<!DOCTYPE html>
+	<html lang="en">
+		<head>
+			<meta charset="UTF-8" />
+			<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+			${head}
+			<script type="module">${hydrate}</script>
+			<style>${css}</style>
+		</head>
+		<body>
+			${html}
+		</body>
+	</html>
 `;
 
 const islands = (base = "") => `
@@ -32,20 +30,19 @@ const hydrate = async (target) => {
 		const name = target.getAttribute("name");
 		const props = JSON.parse(target.getAttribute("props") ?? "{}");
 		const load = performance.now();
-		
+
 		const Component = (await import('${
 	normalize(`/${base}/components/`)
 }' + name + '.island.js')).default;
-		console.info(\`Loaded \${name} in \${performance.now() - load}ms\`, props);
-		
+		console.group(name);
+		console.info(\`Loaded in %c\${Math.round((performance.now() - load) * 1000) / 1000}ms\`, "color: orange");
+
 		const hydrate = performance.now();
 		new Component({ target, props, hydrate: true });
 		target.setAttribute("foraged", "");
 
-		console.info(
-			\`Hydrated %c\${name}%c in %c\${performance.now() - hydrate}%cms\`,
-			"color: darkslategrey; background: floralwhite; font-weight: bold;",
-			"color: inherit", "color: hsl(6, 60%, 18%)", "color: inherit");
+		console.info(\`Hydrated in %c\${Math.round((performance.now() - hydrate) * 1000) / 1000}ms%c with\`, "color: orange", "color: reset", props);
+		console.groupEnd();
 	} catch (_) {
 		console.error(_)
 	}

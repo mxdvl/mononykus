a10293a4137535f75fa33af1086a03ffaf933086
Alex Sanders <alex@sndrs.dev>
2023-04-25 17:00:06 +0100

    add subdir components

--- src/_site/routes/nested-islands.svelte
+++ src/_site/routes/nested-islands.svelte
@@ -1,9 +1,11 @@
 <script>
 	import RedThenBlue from '../components/RedThenBlue.island.svelte'
 	import GreenThenPink from '../components/GreenThenPink.island.svelte'
+	import SubDir from '../components/sub_dir/SubDir.island.svelte'
 </script>
 
 Example of nested islands.
 
 <RedThenBlue />
 <GreenThenPink />
+<SubDir />

39657bc6fa22ee2370763ff9728b1fec4b92ef45
Alex Sanders <alex@sndrs.dev>
2023-04-28 12:38:03 +0100

    use `onMount`

--- src/_site/components/GreenThenPink.island.svelte
+++ src/_site/components/GreenThenPink.island.svelte
@@ -1,13 +1,15 @@
 <script>
+	import { onMount } from 'svelte'
+
 	let green = true
 	let pink = false
 
-	setTimeout(() => {
-		green = false
-		pink = true
-	}, 1000)
-
-	console.log('hello from GreenThenPink.island.svelte')
+	onMount(() => {
+		setTimeout(() => {
+			green = false
+			pink = true
+		}, 1000)
+	})
 </script>
 
 <p class:green class:pink>green then pink</p>

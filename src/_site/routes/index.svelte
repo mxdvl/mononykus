d7de96325e1ffd44fcb6d854b9679ca58b826d7a
Max Duval <hi@mxdvl.com>
2023-06-09 16:20:02 +0100

    refactor: serve image comparison on index

--- src/_site/routes/index.svelte
+++ src/_site/routes/index.svelte
@@ -1,8 +1,5 @@
 <script>
-  import Header from "../components/Header.svelte";
-  import HelloFriend from "../components/HelloFriend.island.svelte";
-  import Counter from "../components/Counter.island.svelte";
-  import Body from "../components/Body.svelte";
+  import Images from "../components/Images.island.svelte";
 </script>
 
 <svelte:head>
@@ -13,55 +10,26 @@
   />
 </svelte:head>
 
-<Body>
-  <Header />
+<body>
   <main>
-    <h1>Mononykus: a svelte dinosaur</h1>
-    <h2>Partial Hydration with Svelte and Deno</h2>
+    <h1>Image Comparison tool</h1>
+    <h2>Compare various image qualities and formats</h2>
 
-    <p>
-      Working on project build in Svelte and Deno is refreshing, because the
-      amount of configuration is minimal, performance is excellent and results
-      are predictable.
-    </p>
-
-    <p>
-      <a href="https://kit.svelte.dev">SvelteKit</a>
-      has decided to go the route of full hydration, and
-      <a href="https://astro.build/">Astro</a> still requires Vite and the Node ecosystem.
-      This is a lightweight alternative to these approaches. This is mainly a way
-      of exploring these concepts to understand what are the tradeoffs.
-    </p>
-
-    <p>
-      Here, we choose to hydrate individual “islands”, similar to the approach
-      taken by Astro and <a href="https://elderguide.com/tech/elderjs/"
-        >Elder.js</a
-      >. For example. the greeting below responds to user input on device that
-      have JavaScript enabled.
-    </p>
-
-    <div class="island">
-      <HelloFriend value="late Cretaceous friend" />
-    </div>
-
-    <div class="island">
-      <Counter />
-    </div>
+    <Images />
   </main>
-</Body>
+</body>
 
 <style>
-  main {
-    padding: 0 3rem;
-    max-width: 30em;
-    margin: auto;
+  :global(:root) {
+    color-scheme: dark;
+    font-family: ui-monospace, 'Cascadia Code', 'Source Code Pro', Menlo, Consolas, 'DejaVu Sans Mono', monospace;
   }
-
-  .island {
-    border: 2px solid antiquewhite;
-    border-radius: 0.25rem;
-    padding: 0.5rem;
-    margin: 1rem -0.5rem;
+  body {
+    background-color: #112;
+    color: #ccc;
+  }
+  main {
+    padding: 0 1rem;
+    margin: auto;
   }
 </style>

7e89229eebe36d6fb3faaf34cdb69510f55e2dc2
Max Duval <hi@mxdvl.com>
2023-07-11 10:28:46 +0100

    chore: bump esbuild version

--- src/esbuild_plugins/svelte_internal.ts
+++ src/esbuild_plugins/svelte_internal.ts
@@ -1,4 +1,4 @@
-import type { Plugin } from "https://deno.land/x/esbuild@v0.17.16/mod.js";
+import type { Plugin } from "https://deno.land/x/esbuild@v0.18.11/mod.js";
 
 const contents = await fetch(
 	"https://cdn.jsdelivr.net/npm/svelte@3.58.0/internal/index.mjs",

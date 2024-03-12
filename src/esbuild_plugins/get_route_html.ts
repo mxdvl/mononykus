9e5723f00ea6a4a9f914a7d4a4d2c8f5b42d4ad7
Max Duval <hi@mxdvl.com>
2024-03-12 15:32:03 +0000

    fix(html): no unnecessary indent

--- src/esbuild_plugins/get_route_html.ts
+++ src/esbuild_plugins/get_route_html.ts
@@ -6,20 +6,20 @@ interface TemplateOptions {
 	html: string;
 }
 
-const template = ({ css, head, html }: TemplateOptions) => `
-	<!DOCTYPE html>
-	<html lang="en">
-		<head>
-			<meta charset="UTF-8" />
-			<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-			${head}
-			<style>${css}</style>
-		</head>
-		<body>
-			${html}
-		</body>
-	</html>
+const template = ({ css, head, html }: TemplateOptions) =>
+	`<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		${head}
+		<style>${css}</style>
+	</head>
+	<body>
+		${html}
+	</body>
+</html>
 `;
 
 export const get_route_html = ({ html, css, head }: {

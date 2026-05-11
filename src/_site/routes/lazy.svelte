c06f4525664bf9cf4bb900bf81b464e3a746b8cd
Max Duval <hi@mxdvl.com>
2026-05-11 18:17:09 +0100

    test: a lazy file

---
+++ src/_site/routes/lazy.svelte
@@ -0,0 +1,28 @@
+<script>
+    import Header from "../components/Header.svelte";
+    import Body from "../components/Body.svelte";
+
+    const result = await Promise.resolve("Something");
+</script>
+
+<svelte:head>
+    <title>Mononykus – Deno + Svelte (lazy)</title>
+</svelte:head>
+
+<Body>
+    <Header />
+    <main>
+        <h1>Mononykus: supports <code>await</code> in Svelte</h1>
+
+        <p>{result}</p>
+        <!-- <p>{await Promise.resolve("This is async")}</p> -->
+    </main>
+</Body>
+
+<style>
+    main {
+        padding: 0 3rem;
+        max-width: 30em;
+        margin: auto;
+    }
+</style>
